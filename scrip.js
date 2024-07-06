dcbecde15224d6f7995e40467c80b71c25e723b7
MusaSiddiquee <musasiddiquee71@gmail.com>
2024-07-06 08:31:52 -0700

    first commit

---
+++ scrip.js
@@ -0,0 +1,84 @@
+alert ("Musa")
+
+var num1=+ prompt("Enter your 1 first number",)
+
+var num2=+ prompt("Enter your 2 second number",)
+
+var num3=+ prompt("Enter your 3 third number",)
+
+alert(num1 + num2 + num3)
+
+var num = prompt("Enter any number")
+alert(num + " X " + "1" + "=" + num * 1)
+alert(num + " X " + "2" + "=" + num * 2)
+alert(num + " X " + "3" + "=" + num * 3)
+alert(num + " X " + "4" + "=" + num * 4)
+alert(num + " X " + "5" + "=" + num * 5)
+alert(num + " X " + "6" + "=" + num * 6)
+alert(num + " X " + "7" + "=" + num * 7)
+alert(num + " X " + "8" + "=" + num * 8)
+alert(num + " X " + "9" + "=" + num * 9)
+alert(num + " X " + "10" + "=" + num * 10)
+
+
+
+var gmail = prompt  ("Enter your gmail")
+var password = prompt (" password")
+
+if ( gmail === "kashan@gmail.com" && password === "admin"){
+        alert("you are kashan")
+    }
+    else(
+        alert("you are loser")
+    )                                       
+
+var question1=+prompt("What is your name?")
+
+var answer1="musa"
+
+var question2=+prompt("What is your hobby?")
+
+var answer2="gaming"
+
+var question3=+prompt("What is your fav moovie?")
+
+var answer3="Avangers and game"
+
+if(question1===answer1)
+    {number++}
+
+
+if(question2===answer2)
+    {number++}
+
+
+if(question3===answer3)
+    {number++}
+
+
+var lastnum =+ prompt  (question1+question2+question3)
+
+var gmail = prompt ("enter your gmail")
+
+if (gmail === "Musa@gmail.com"){
+    var password = prompt ("enter your password")
+    if(password === "html")
+    {
+    alert("you are Musa")
+    }
+}   
+else{
+    alert("you are user")
+}
+
+
+var schools = ["city school","millenniums","grammar"]
+
+var school =prompt("enter your school name")
+if(schools[0]===school){alert("this scool is best  "+ schools [0])}
+if(schools[1]===school){alert("this scool is best  "+ schools [1])}
+if(schools[2]===school){alert("this scool is best  "+ schools [2])}
+
+else{
+    alert("this is not in the collection")
+}
